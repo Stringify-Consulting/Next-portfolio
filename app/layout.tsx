95ec93013daf088f3687fe5e2e417d0a7eee7371
Ajayi AKINSOLA MARTINS <145346225+martinsAkin@users.noreply.github.com>
2025-04-12 10:06:24 +0100

    Update layout.tsx to handle font usage locally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,8 @@
 import './globals.css';
-import { Poppins, Bruno_Ace, Figtree } from "next/font/google";
-
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-
-import "./globals.css";
 import { ReactNode } from "react";
 // import { LayoutProps } from "framer-motion";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
